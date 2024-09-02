231827533dd946ea45ab41f78d78e00ac19ef2da
Alex-PC <alex_mar1999@yahoo.com>
2024-09-02 20:44:32 +0300

    update main content editors page

--- BlazorCleanArchitectureTest/WebUI/Constants/UIConstants.cs
+++ BlazorCleanArchitectureTest/WebUI/Constants/UIConstants.cs
@@ -4,4 +4,9 @@ public static class UIConstants
 {
     public const string SuccessfulRegister = "Account succesfuly created!";
     public const string ChangeRoleSuccessfully = "User role changed succesfully!";
+    public const string Instruction = "Are you sure you want to do this?";
+
+    public const string AddVehicleOwnerTitle = "Add Vehicle Owner";
+    public const string AddVehicleBrandTitle = "Add Vehicle Brand";
+    public const string AddVehicleTitle = "Add Vehicle";
 }
